bb7a10d0461ac2fdb704487c8cf9714df04e9ed0
sambodjango <gringolalia@gmail.com>
2021-01-22 11:09:18 -0300

    deploy

---
+++ precache-manifest.6b4e2d9c9948864b8155e075aa25ec22.js
@@ -0,0 +1,46 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "6a1d2c69810b8f9502d7",
+    "url": "/css/about.f46b00f1.css"
+  },
+  {
+    "revision": "608a3a31dfb1fc0fd47a",
+    "url": "/css/app.5fcddd21.css"
+  },
+  {
+    "revision": "5459b253ddc07357fe25",
+    "url": "/css/chunk-vendors.e33783dd.css"
+  },
+  {
+    "revision": "7226a46e7f28a60e289da005060e2db6",
+    "url": "/img/enigma.7226a46e.jpg"
+  },
+  {
+    "revision": "3fea563c5316b0039389b59f0a876105",
+    "url": "/img/hammer-sickle.3fea563c.svg"
+  },
+  {
+    "revision": "0c5bb5cd29071290be91f174a5fd07e1",
+    "url": "/index.html"
+  },
+  {
+    "revision": "6a1d2c69810b8f9502d7",
+    "url": "/js/about.8599bea7.js"
+  },
+  {
+    "revision": "608a3a31dfb1fc0fd47a",
+    "url": "/js/app.87b9e517.js"
+  },
+  {
+    "revision": "5459b253ddc07357fe25",
+    "url": "/js/chunk-vendors.9d3eb740.js"
+  },
+  {
+    "revision": "5db442a300b2657dee51e4df9eb8548c",
+    "url": "/manifest.json"
+  },
+  {
+    "revision": "b6216d61c03e6ce0c9aea6ca7808f7ca",
+    "url": "/robots.txt"
+  }
+]);
